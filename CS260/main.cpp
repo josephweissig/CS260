d8b831986b69a2bc8e1548514aeb292a8a524601
Joseph Weissig <admin@jweissig.com>
2024-04-02 12:12:39 -0700

    Initial Commit

---
+++ CS260/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CS260
+//
+//  Created by Gold Weissig on 4/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
